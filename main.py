4f47472080452a1cc61b7e6f93475ecdcc90c3b8
David Sang <dvdsang47@gmail.com>
2023-03-24 16:35:21 -0700

    added CharacterObjects and screen size

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
 import pygame
+pygame.init()
+
+BLACK = (0, 0, 0)
+
+size = (600, 600)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Game")
+
+clock = pygame.time.Clock()
+
+gameIsRunning = True
+
+while gameIsRunning:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameIsRunning = False
+
+    screen.fill(BLACK)
+
+    # Update the screen
+    pygame.display.flip()
+
+    # 60 FPS
+    clock.tick(60)
+
+# Quit when game loop ends
+pygame.quit()
 
 print("hello")
 print("bye")
