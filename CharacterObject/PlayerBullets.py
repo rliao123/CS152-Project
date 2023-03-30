00f67d138b5742304f883789b1edb5696a56be80
David Sang <dvdsang47@gmail.com>
2023-03-30 14:59:09 -0700

    updated Z-hold and player hitbox, and colors

--- CharacterObject/PlayerBullets.py
+++ CharacterObject/PlayerBullets.py
@@ -6,7 +6,7 @@ import random
 class PlayerBullets(pygame.sprite.Sprite):
     def __init__(self, posX, posY):
         super().__init__()
-        self.image = pygame.image.load("CS152-Project/images/player bullet.png")
+        self.image = pygame.image.load("images/player bullet.png")
         self.rect = self.image.get_rect()
         self.rect.center = (posX, posY)
 
