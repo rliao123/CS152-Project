5d6eedbf0830974fe985d80d9696dfdc832e3a49
David Sang <dvdsang47@gmail.com>
2023-03-30 16:07:56 -0700

    parameterized bullet patterns & added new enemy patterns

--- CharacterObject/EnemyBullets.py
+++ CharacterObject/EnemyBullets.py
@@ -3,7 +3,7 @@ import math
 
 
 class EnemyBullets(pygame.sprite.Sprite):
-    def __init__(self, x, y, angle):
+    def __init__(self, x, y, angle, speed):
         super().__init__()
         self.image = pygame.Surface([15, 15])
         self.image.fill((255, 255, 255))
@@ -12,10 +12,11 @@ class EnemyBullets(pygame.sprite.Sprite):
         self.x = x
         self.y = y
         self.angle = angle
+        self.speed = speed
 
     def update(self):
-        self.rect.x += 4 * math.cos(math.radians(self.angle))
-        self.rect.y += 4 * math.sin(math.radians(self.angle))
+        self.rect.x += self.speed * math.cos(math.radians(self.angle))
+        self.rect.y += self.speed * math.sin(math.radians(self.angle))
         if self.rect.top < 0 or self.rect.bottom > 600 or self.rect.right > 600 or self.rect.left < 0:
             self.kill()
 
