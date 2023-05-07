ccd539ce1d6f5ab3b0fd71b4ec6a4a2106ee8e5a
rliao123 <92598518+rliao123@users.noreply.github.com>
2023-05-06 18:13:54 -0700

    added comments/docstrings for player and player bullets

--- CharacterObject/PlayerBullets.py
+++ CharacterObject/PlayerBullets.py
@@ -4,13 +4,22 @@ import random
 
 
 class PlayerBullets(pygame.sprite.Sprite):
+    """A class used to represent Player Bullet"""
     def __init__(self, posX, posY):
+        """Initializes Player Bullet object
+        posX -- center x coordinate of player bullet
+        posY -- center y coordinate of player bullet
+        """
+
         super().__init__()
         self.image = pygame.image.load("images/player bullet.png")
         self.rect = self.image.get_rect()
         self.rect.center = (posX, posY)
 
     def update(self):
+        """Moves player bullet up the screen"""
+
         self.rect.move_ip(0, -5)
+        # destroy bullet when it goes outside the screen's borders
         if self.rect.top < 0:
             self.kill()
